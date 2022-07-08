867904f5f8b8e0addbb4d37ffd4a639227eef16a
Kate Shpakovskaya <kate.shpakovskaya02@gmail.com>
2022-07-08 11:00:33 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  let stack = [];
-	for (let i = 0; i<str.length; i++){
-		bracketsConfig.forEach((x,y) => {
-			if (str[i] === x[0]){
-				stack.push(y)
-			} else if (str[i] === x[1] && stack[stack.length-1] === y){
-				stack.pop()
-			} else return false;
-		});
-	}
+	let stack = [];
+	  for (let i = 0; i<str.length; i++){
+	  for (let y = 0; y < bracketsConfig.length; y++) {
+		if (str[i] === bracketsConfig[y][0] && str[i] === bracketsConfig[y][1] ) {
+		stack[stack.length-1] !== y ? stack.push(y) :  stack.pop()
+		}
+			  else if (str[i] === bracketsConfig[y][0]){
+				  stack.push(y)
+			  } else if (str[i] === bracketsConfig[y][1]){
+				if (stack[stack.length-1] === y) {
+				  stack.pop()
+				} else return false
+			  } 
+	  }
+	  }
 	return stack.length === 0 ? true : false;
-}
+  }
+
