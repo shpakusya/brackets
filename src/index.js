9a592a04c53e264af46404588e4d70134a3dfd22
Kate Shpakovskaya <kate.shpakovskaya02@gmail.com>
2022-07-08 09:57:38 +0300

    feat: complete for normal brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+	for (let i = 0; i<str.length; i++){
+		bracketsConfig.forEach((x,y) => {
+			if (str[i] === x[0]){
+				stack.push(y)
+			} else if (str[i] === x[1] && stack[stack.length-1] === y){
+				stack.pop()
+			} else return false;
+		});
+	}
+	return stack.length === 0 ? true : false;
 }
